7f66195c63a88e7a34a9b6bfab0a810d8954c373
Carlos Aviles Buitron <carlosavilesbuitron@carloss-mbp.myfiosgateway.com>
2022-10-21 15:04:28 -0400

    Setup styling framework

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h1>Welcome to CosmoShare</h1>
-    </div>
-  );
-};
+	return (
+		<div>
+			<h1 className='text-green-500 text-4xl flex'>Welcome to CosmoShare</h1>
+		</div>
+	)
+}
 
-export default Home;
+export default Home
