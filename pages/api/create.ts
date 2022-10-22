dbc780da716a0454fcf9dfcb4f650b47df55a6f4
Carlos Aviles Buitron <carlosavilesbuitron@carloss-mbp.myfiosgateway.com>
2022-10-22 12:02:32 -0400

    Create test launch contract function

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -1,9 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { ThirdwebSDK } from '@thirdweb-dev/sdk'
 
 type Data = {
 	status: string
+	message?: string
+	content?: any
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-	res.status(200).json({ status: 'success' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+	try {
+		const address = '0xb28d5a92605198fa66a577d7ab2dbbf1a0e78f2b'
+		const sdk = new ThirdwebSDK('mumbai')
+		const contractAddress = await sdk.deployer.deploySplit({
+			name: 'My Split',
+			recipients: [
+				{
+					address: address,
+					sharesBps: 100 * 100,
+				},
+			],
+		})
+		res.status(200).json({
+			status: 'success',
+			message: 'Contract deployed',
+			content: contractAddress,
+		})
+	} catch (e) {
+		console.log(e)
+		res.status(200).json({
+			status: 'error',
+			message: 'Contract failed to deply',
+		})
+	}
 }
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -12,11 +12,24 @@ const Dashboard: NextPage = () => {
 		router.push('/')
 	}
 
+	const handleCreate = async () => {
+		try {
+			const results = await fetch('/api/create')
+			const resultsJson = await results.json()
+			console.log(resultsJson)
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
 	return (
 		<div className='h-full flex flex-col items-center justify-center'>
 			<h1 className='text-green-500 text-center text-4xl'>Dashboard</h1>
 			<p>User: {address}</p>
 			<button onClick={handleDisconnect}>Logout</button>
+			<br />
+			<br />
+			<button onClick={handleCreate}>Create Contract</button>
 		</div>
 	)
 }
